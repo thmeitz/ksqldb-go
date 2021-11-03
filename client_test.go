2130e0bbbb4930bd7667b0cc1815129ba0530bf0
Thomas Meitz <info@jproxx.com>
2021-11-03 19:54:56 +0100

    [client] tests

--- client_test.go
+++ client_test.go
@@ -18,23 +18,36 @@ package ksqldb_test
 
 import (
 	"testing"
+
+	"github.com/Masterminds/log-go/impl/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/thmeitz/ksqldb-go"
 )
 
-// var (
-// 	logger = logrus.NewStandard()
-// )
+var (
+	logger = logrus.NewStandard()
+)
 
 func TestClientNotNil(t *testing.T) {
-	// client := ksqldb.NewClient("http://example.com", "testuser", "testpassword", logger)
-	// assert.NotNil(t, client)
+	client, _ := ksqldb.NewClient(ksqldb.Options{}, logger)
+	assert.NotNil(t, client)
+}
+
+func TestClientPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The Client did not panic")
+		}
+	}()
+	ksqldb.NewClient(ksqldb.Options{BaseUrl: "sf"}, logger)
 }
 
 func TestClientSanitizeQuery(t *testing.T) {
-	//client := ksqldb.NewClient("http://example.com", "testuser", "testpassword", logger)
-	//	sanitizedString := client.SanitizeQuery(`
-	//
-	//	This is the 	house of Nicolas
-	//
-	//`)
-	//	assert.Equal(t, "This is the house of Nicolas", sanitizedString)
+	client, _ := ksqldb.NewClient(ksqldb.Options{}, logger)
+	sanitizedString := client.SanitizeQuery(`
+	
+		This is the 	house of Nicolas
+	
+	`)
+	assert.Equal(t, "This is the house of Nicolas", sanitizedString)
 }
--- internal/utils_test.go
+++ internal/utils_test.go
@@ -12,12 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Parts of this apiclient are borrowed from Zalando Skipper
-https://github.com/zalando/skipper/blob/master/net/httpclient.go
-
-Zalando licence: MIT
-https://github.com/zalando/skipper/blob/master/LICENSE
 */
 
 package internal_test
@@ -32,25 +26,23 @@ import (
 var tests = []struct {
 	name    string
 	url     string
-	want    bool
 	message string
 }{
-	{"empty url", "", false, "invalid host name given"},
-	{"check localhost with port", "http://localhost:8123", true, ""},
-	{"check localhost without port", "http://localhost", true, ""},
-	{"invalid url scheme", "httpx://google.com", false, "invalid url scheme given"},
-	{"invalid protocol", "stomp://localhost", false, "invalid url scheme given"},
-	{"invalid host", "httpx://", false, "invalid host name given"},
-	{"empty url scheme", "://hostname", false, "can't parse url: parse \"://hostname\": missing protocol scheme"},
-	{"check url parser", "http://ahsd^öf023as", false, "can't parse url: parse \"http://ahsd^öf023as\": invalid character \"^\" in host name"},
+	{"empty url", "", "invalid host name given"},
+	{"check localhost with port", "http://localhost:8123", ""},
+	{"check localhost without port", "http://localhost", ""},
+	{"invalid url scheme", "httpx://google.com", "invalid url scheme given"},
+	{"invalid protocol", "stomp://localhost", "invalid url scheme given"},
+	{"invalid host", "httpx://", "invalid host name given"},
+	{"empty url scheme", "://hostname", "can't parse url: parse \"://hostname\": missing protocol scheme"},
+	{"check url parser", "http://ahsd^öf023as", "can't parse url: parse \"http://ahsd^öf023as\": invalid character \"^\" in host name"},
 }
 
 func TestGetUrlValid(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, valid, err := internal.GetUrl(tt.url)
+			_, err := internal.GetUrl(tt.url)
 			// fmt.Println(fmt.Sprintf("%v. %v: %v: %v => isValid:%v error: %v", idx, tt.name, tt.url, tt.want, valid, err))
-			assert.Equal(t, tt.want, valid, "they should be equal")
 			if err != nil {
 				assert.Equal(t, tt.message, err.Error())
 			}
