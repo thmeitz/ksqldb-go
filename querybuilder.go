343abbe0d5249a6eca4fbf3c2c2f8269724f6e62
Thomas Meitz <info@jproxx.com>
2021-11-05 15:32:45 +0100

    first version of QueryBuilder

--- querybuilder.go
+++ querybuilder.go
@@ -1,1 +1,84 @@
-package ksqldb_test
+package ksqldb
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	QBErr           = "qbErr"
+	EMPTY_STATEMENT = "empty ksql statement"
+)
+
+type QueryBuilder struct {
+	stmt      string
+	origStmnt string
+	lastErr   error
+	options   *QueryBuilderOptions
+	ctx       context.Context
+}
+
+// QueryBuilderOptions type
+type QueryBuilderOptions struct {
+	Parse   bool // set to true, to parse the sql by KSQLParser
+	Context context.Context
+}
+
+// DefaultQueryBuilder returns a @QueryBuilder with default values
+func DefaultQueryBuilder(stmnt string) (*QueryBuilder, error) {
+	context := context.Background()
+	options := QueryBuilderOptions{Parse: false}
+
+	if err := checkEmptyStatement(stmnt); err != nil {
+		return nil, err
+	}
+
+	return &QueryBuilder{ctx: context, stmt: stmnt, origStmnt: stmnt, options: &options}, nil
+}
+
+// QueryBuilderWithOptions returns a @QueryBuilder, which can be configured by @QueryBuilderOptions
+func QueryBuilderWithOptions(stmnt string, options *QueryBuilderOptions) (*QueryBuilder, error) {
+	ctx := context.Background()
+
+	if err := checkEmptyStatement(stmnt); err != nil {
+		return nil, err
+	}
+
+	if options != nil && options.Context != nil {
+		ctx = options.Context
+	}
+
+	return &QueryBuilder{ctx: ctx, stmt: stmnt, origStmnt: stmnt, options: options}, nil
+}
+
+// GetInputStatement gets the original statement given do @DefaultQueryBuilder or @QueryBuilderWithOptions
+func (qb *QueryBuilder) GetInputStatement() string {
+	return qb.stmt
+}
+
+// Bind parameters to QueryBuilder
+func (qb *QueryBuilder) Bind(params ...interface{}) (*string, error) {
+	paramCount := len(params)
+	count := strings.Count(qb.origStmnt, "?")
+	if paramCount < count {
+		return nil, fmt.Errorf("%v: %v", QBErr, "to few params")
+	} else if paramCount > count {
+		return nil, fmt.Errorf("%v: %v", QBErr, "to many params")
+	}
+
+	for idx, param := range params {
+		fmt.Println(idx, ":", param)
+		qb.stmt = strings.Replace(qb.stmt, "?", strconv.FormatInt(int64(param.(int)), 10), 1)
+	}
+	return &qb.stmt, nil
+}
+
+// checkEmptyStatement to keep the Factories dry
+func checkEmptyStatement(stmnt string) error {
+	if stmnt == "" {
+		return fmt.Errorf("qbErr: %v", EMPTY_STATEMENT)
+	}
+	return nil
+}
--- querybuilder_test.go
+++ querybuilder_test.go
@@ -1,1 +1,75 @@
 package ksqldb_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thmeitz/ksqldb-go"
+)
+
+const (
+	SelectBla = "select * from bla where id=?"
+)
+
+func TestDefaultQueryBuilder_NotNil(t *testing.T) {
+	builder, err := ksqldb.DefaultQueryBuilder(SelectBla)
+	assert.Nil(t, err)
+	assert.NotNil(t, builder)
+}
+
+func TestDefaultQueryBuilder_EmptyStatement(t *testing.T) {
+	builder, err := ksqldb.DefaultQueryBuilder("")
+	assert.Nil(t, builder)
+	assert.NotNil(t, err)
+	assert.Equal(t, "qbErr: empty ksql statement", err.Error())
+}
+
+func TestDefaultQueryBuilder_GetStatement(t *testing.T) {
+	builder, _ := ksqldb.DefaultQueryBuilder(SelectBla)
+	assert.NotNil(t, builder)
+	stmnt := builder.GetInputStatement()
+	assert.Equal(t, SelectBla, stmnt)
+}
+
+func TestQueryBuilderWithOptions_EmptyStatement_NilOptions(t *testing.T) {
+	builder, err := ksqldb.QueryBuilderWithOptions("", nil)
+	assert.Nil(t, builder)
+	assert.NotNil(t, err)
+}
+
+func TestQueryBuilderWithOptions_EmptyOptions(t *testing.T) {
+	options := ksqldb.QueryBuilderOptions{}
+	builder, err := ksqldb.QueryBuilderWithOptions(SelectBla, &options)
+	assert.NotNil(t, builder)
+	assert.Nil(t, err)
+}
+
+func TestQueryBuilderWithOptions_WithContext(t *testing.T) {
+	options := ksqldb.QueryBuilderOptions{Context: context.Background()}
+	builder, err := ksqldb.QueryBuilderWithOptions(SelectBla, &options)
+	assert.NotNil(t, builder)
+	assert.Nil(t, err)
+}
+
+func TestQueryBuilder_Bind_ToManyParamsError(t *testing.T) {
+	builder, _ := ksqldb.DefaultQueryBuilder(SelectBla)
+	_, err := builder.Bind(1, "bla", 31235)
+	assert.NotNil(t, err)
+	assert.Equal(t, "qbErr: to many params", err.Error())
+}
+
+func TestQueryBuilder_Bind_ToFewParamsError(t *testing.T) {
+	builder, _ := ksqldb.DefaultQueryBuilder(SelectBla)
+	_, err := builder.Bind()
+	assert.NotNil(t, err)
+	assert.Equal(t, "qbErr: to few params", err.Error())
+}
+
+func TestQueryBuilder_Bind_CorrectParams(t *testing.T) {
+	builder, _ := ksqldb.DefaultQueryBuilder(SelectBla)
+	stmnt, err := builder.Bind(1)
+	assert.Nil(t, err)
+	assert.NotNil(t, stmnt)
+	assert.Equal(t, "select * from bla where id=1", *stmnt)
+}
