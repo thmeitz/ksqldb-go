aad28e35adb4e9ed4aca58b10bd3070a9db8d7ff
Thomas Meitz <info@jproxx.com>
2021-11-09 19:49:49 +0100

    [parser] added tests

---
+++ parser/ksql_syntax_error_test.go
@@ -0,0 +1,45 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thmeitz/ksqldb-go/parser"
+)
+
+func TestKSqlSyntaxError_ErrorFunc(t *testing.T) {
+	err := parser.KSqlSyntaxError{
+		Line:   1,
+		Column: 10,
+		Msg:    "dummy message",
+	}
+	assert.Equal(t, "error on line(1):column(10): dummy message", err.Error())
+}
+
+func TestKSqlSyntaxErrorList_ErrorfFunc(t *testing.T) {
+	var errorList parser.KSqlSyntaxErrorList = []parser.KSqlSyntaxError{
+		{
+			Line:   1,
+			Column: 10,
+			Msg:    "dummy message",
+		},
+	}
+	assert.Equal(t, "1 sql syntax error(s) found", errorList.Error())
+}
+
+func TestKSqlErrorListener_HasErrors_ErrorCount(t *testing.T) {
+	listener := parser.KSqlErrorListener{}
+	assert.False(t, listener.HasErrors())
+	assert.Equal(t, 0, listener.ErrorCount())
+}
+
+func TestKSqlErrorListener_HSyntaxError(t *testing.T) {
+	listener := parser.KSqlErrorListener{}
+	assert.False(t, listener.HasErrors())
+	assert.Equal(t, 0, listener.ErrorCount())
+
+	//recognizer := antlr.NewBaseRecognizer()
+
+	//listener.SyntaxError(*recognizer, nil, 1, 10, "some error", nil)
+
+}
